f388d6b06ea62e9097db2864177b127aff18942a
高境杉 <1359287622@qq.com>
2020-04-28 17:40:55 +0800

    nodejs

--- 06_依赖管理学习/app.js
+++ 06_依赖管理学习/app.js
@@ -1,5 +1,6 @@
 var sl = require('solarlunar');
 var rmb = require('rmb-x');
+var py=require('Chinese-to-Pinyin');
 
 // 农历对象
 var obj = sl.solar2lunar(2020, 9, 20);
@@ -8,3 +9,6 @@ console.log(obj.dayCn);
 
 // 人民币大写
 console.log(rmb(234234));
+
+// 输出汉字的拼音
+console.log(py('我是小可爱'));  
