4062feaa6192f6309597985f3e208c49bd62ca98
高境杉 <1359287622@qq.com>
2020-04-30 09:25:21 +0800

    nodejs

--- 11_Express POST/app.js
+++ 11_Express POST/app.js
@@ -1,9 +1,9 @@
 var express = require('express');
-// url模块是内置模块，不需要装依赖，用来拆解URL的
-var url = require('url');
-var formidable = require('formidable');
 
-var fs=require('fs');
+// formidable是用来处理POST请求的
+var formidable = require('formidable');
+// 引入fs模块，写文件
+var fs = require('fs');
 
 // 创建app
 var app = express();
@@ -15,17 +15,29 @@ var app = express();
 // app.use('/www',express.static('www'));
 app.use(express.static('www'));
 
+// 当用户用post请求访问提交的时候，做的事情
 app.post('/tijiao', function (req, res) {
+  // 用formidable这个第三方npm包，来处理POST请求的参数
+  // https://www.npmjs.com/package/formidable
   const form = formidable({multiples: true});
 
   form.parse(req, (err, fields, files) => {
+    // fields参数就是HTTP的报文体中的信息
     console.log(fields);
-    
-    fs.appendFile('/存储.txt',`
-    ${fields.name}
-    `,function(){
-      res.send('<h1>我们已妥收您的信息</h1>')
-    })
+    // 追加文件
+    fs.appendFile(
+      './存储.txt',
+      `
+      [姓名] ${fields.name}
+      [性别] ${fields.age}
+      [年龄] ${fields.sex}
+      ************************************
+    `,
+      function () {
+        // 回调函数，表示当写完文件之后做的事情
+        res.send('<h1>我们已妥收您的信息</h1>');
+      }
+    );
   });
 });
 
