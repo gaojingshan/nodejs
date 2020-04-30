8be4ff51f8f190d046026e01273d32aee18895a2
高境杉 <1359287622@qq.com>
2020-04-30 21:22:25 +0800

    nodejs

--- 13_Make analog interface/app.js
+++ 13_Make analog interface/app.js
@@ -1,27 +1,29 @@
 var express = require('express');
-// url模块是内置模块，不需要装依赖，用来拆解URL的
-var url = require('url');
-
-// formidable是用来处理POST请求的
-var formidable = require('formidable');
-
-// 内置了一个模块
-var path = require('path');
-
-// 引入fs模块，写文件
-var fs = require('fs');
 
 // 引入mockjs依赖
 var mockjs = require('mockjs');
 
+// 创建随机对象
+var Random = mockjs.Random;
+
 // 创建app
 var app = express();
 
-// 只需要写一句话，就能够实现静态化文件夹
-// 什么叫做静态化呢？就是能够显示不需要预编译的文件html、js、css、 jpg、png
-// use表示使用插件
-// 说白了，就是这个文件夹中的所有内容都会有自动的路由。www就是文件夹的名字。
-app.use(express.static('www'));
+app.get('/students', function (req, res) {
+  // 构建一个数组
+  var arr = [];
+  // 推入10项
+  for (var i = 0; i < 10; i++) {
+    arr.push({
+      name: Random.cname(),
+      age: Random.integer(20, 40),
+      sex: Random.pick(['男', '女']),
+    });
+  }
+  // 输出JSON
+  res.json(arr)
+});
+
 
 // 监听3000端口
 app.listen(3000);
