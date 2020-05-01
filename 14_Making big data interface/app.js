e2cb32ceac5f262b97d3195ed5f55d08ed85b291
高境杉 <1359287622@qq.com>
2020-05-01 11:03:16 +0800

    nodejs

--- 14_Making big data interface/app.js
+++ 14_Making big data interface/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
+// url模块是内置模块，不需要装依赖，用来拆解URL的
+var url = require('url');
 
+// 连接数据库，https://www.npmjs.com/package/mysql
 var mysql = require('mysql');
-// 连接数据库
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -10,9 +12,88 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
+// 创建app
 var app = express();
 
 app.get('/students', function (req, res) {
+  // 得到各种GET请求参数
+  var sex = url.parse(req.url, true).query.sex;
+  var blood = url.parse(req.url, true).query.blood;
+  var star = url.parse(req.url, true).query.star;
+  var ethnic = url.parse(req.url, true).query.ethnic;
+  var page = url.parse(req.url, true).query.page;
 
+  // 拼接一个SQL查询
+  var chaxun = [];
+
+  // 如果用户GET请求中有sex
+  if (sex) {
+    chaxun.push('sex="' + sex + '"');
+  }
+
+  // 如果用户GET请求中有blood
+  if (blood) {
+    // 拆分
+    var blood_arr = blood.split('v');
+    var temp = [];
+    // 遍历
+    for (var i = 0; i < blood_arr.length; i++) {
+      temp.push('blood="' + blood_arr[i] + '"');
+    }
+    chaxun.push('(' + temp.join(' OR ') + ')');
+  }
+
+  // 如果用户GET请求中有star
+  if (star) {
+    // 拆分
+    var star_arr = star.split('v');
+    var temp = [];
+    // 遍历
+    for (var i = 0; i < star_arr.length; i++) {
+      temp.push('star="' + star_arr[i] + '"');
+    }
+    chaxun.push('(' + temp.join(' OR ') + ')');
+  }
+
+  // 如果用户GET请求中有ethnic
+  if (ethnic) {
+    // 拆分
+    var ethnic_arr = ethnic.split('v');
+    var temp = [];
+    // 遍历
+    for (var i = 0; i < ethnic_arr.length; i++) {
+      temp.push('ethnic="' + ethnic_arr[i] + '"');
+    }
+    chaxun.push('(' + temp.join(' OR ') + ')');
+  }
+
+  // 如果用户至少有一个查询参数
+  if (chaxun.length != 0) {
+    // 拼接SQL的查询语法
+    var chaxun_str = ' WHERE ' + chaxun.join(' AND ');
+  } else {
+    // 如果用户没有查询参数，就留空
+    var chaxun_str = '';
+  }
+
+  var sql =
+    'SELECT * FROM student ' +
+    chaxun_str +
+    ' ORDER BY id ASC LIMIT ' +
+    (page - 1) * 10 +
+    ', 10';
+  console.log(sql);
+
+  // 执行查询
+  connection.query(
+    {
+      sql: sql,
+    },
+    function (error, results, fields) {
+      res.json(results);
+    }
+  );
 });
+
+// 监听3000端口
 app.listen(3000);
--- 14_Making big data interface/cj.js
+++ 14_Making big data interface/cj.js
@@ -1,8 +1,8 @@
 var mockjs = require('mockjs');
 var Random = mockjs.Random;
 
+// 连接数据库，https://www.npmjs.com/package/mysql
 var mysql = require('mysql');
-// 连接数据库
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -11,12 +11,17 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
+// 计数器
 var count = 0;
+
+// 封装一个函数
 function insert() {
-  if (count == 10) {
-    console.log('全部插入完毕');
+  // 如果次数够了，就结束，如果不写结束语句就会死循环了
+  if (count == 3000) {
+    console.log('已经全部插入完毕');
     return;
   }
+  // 执行一次insert就计数器加1
   count++;
   console.log('正在插入第' + count + '条');
 
@@ -34,6 +39,8 @@ function insert() {
     results,
     fields
   ) {
+    // 插入完一条数据之后的回调，继续插入新的数据，这样的就是同步的了，
+    // 一条接一条插入，不要用for循环套
     insert();
   });
 }
